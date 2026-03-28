ceddaf1559abc4e6da8a6fe869c744558386c9d4
VIVIVIciiEND <akirmandinara@gmail.com>
2026-03-28 20:12:56 +0300

    Step 10: Lifecycle callbacks and rememberSaveable implemented

--- app/src/main/java/com/zhanatpaeva/gradeclicker/MainActivity.kt
+++ app/src/main/java/com/zhanatpaeva/gradeclicker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.zhanatpaeva.gradeclicker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -36,9 +37,11 @@ import com.zhanatpaeva.gradeclicker.model.Grade
 import com.zhanatpaeva.gradeclicker.ui.theme.GradeClickerTheme
 
 
+private const val TAG = "MainActivity"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG , "onCreate Called")
         enableEdgeToEdge()
         setContent {
             GradeClickerTheme {
@@ -49,6 +52,30 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG , "onStart Called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG , "onResume Called")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG , "onRestart Called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG , "onPause() Called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG , "onStop Called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG , "onDestroy Called")
     }
 }
 
